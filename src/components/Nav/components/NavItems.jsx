a9950558a3d5aec2df480c0c4e5e52e56f26bc72
harknoel <bontilao.harknoel@gmail.com>
2024-04-27 19:41:15 +0800

    Improve the design of the popover

--- src/components/Nav/components/NavItems.jsx
+++ src/components/Nav/components/NavItems.jsx
@@ -56,16 +56,38 @@ export default function NavItems() {
   const menuId = 'primary-search-account-menu'
   const renderMenu = (
     <Menu
+      slotProps={{
+        paper: {
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 0.1,
+            '&::before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 20,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        },
+      }}
       anchorEl={anchorEl}
       anchorOrigin={{
-        vertical: 'top',
         horizontal: 'right',
+        vertical: 'bottom',
       }}
       id={menuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
         horizontal: 'right',
+        vertical: 'top',
       }}
       open={isMenuOpen}
       onClose={handleMenuClose}
