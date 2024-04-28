b94758abe82cf262000189d058417af4a02c80e6
herald-noel <bontilao.heraldnoel@gmail.com>
2024-04-28 19:15:13 +0800

    Redesign comment

--- src/pages/Comments/components/Comments.tsx
+++ src/pages/Comments/components/Comments.tsx
@@ -1,16 +1,55 @@
 import React from 'react'
 import {
   Typography,
-  Grid,
   Card,
-  CardActionArea,
   CardContent,
-  CardActions,
+  Menu,
+  MenuItem,
+  Avatar,
+  Box,
 } from '@mui/material'
-import Button from '@mui/material/Button'
-import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+import MoreIcon from '@mui/icons-material/MoreHoriz'
 
 export default function Comments() {
+  const [moreAnchorEl, setMoreAnchorEl] = React.useState(null)
+
+  const mobileMenuId = 'primary-search-account-menu-mobile'
+
+  const isMobileMenuOpen = Boolean(moreAnchorEl)
+
+  const handleMenuOpen = (event) => {
+    setMoreAnchorEl(event.currentTarget)
+  }
+
+  const handleMenuClose = () => {
+    setMoreAnchorEl(null)
+  }
+
+  const renderMenu = (
+    <Menu
+      anchorEl={moreAnchorEl}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem>
+        <Typography variant="body2">Edit Comment</Typography>
+      </MenuItem>
+      <MenuItem>
+        <Typography variant="body2">Delete</Typography>
+      </MenuItem>
+    </Menu>
+  )
   return (
     <Card
       sx={{
@@ -19,12 +58,37 @@ export default function Comments() {
       }}
     >
       <CardContent>
-        <Typography variant="subtitle1">John Doe </Typography>
-
-        <Typography variant="subtitle2" color="text.secondary">
-          March 15,2024
-        </Typography>
-        <span>.</span>
+        <Box
+          sx={{
+            display: {
+              xs: 'flex',
+              justifyContent: 'space-between',
+              marginBottom: '10px',
+            },
+          }}
+        >
+          <Box display={'flex'} alignItems={'center'}>
+            <Avatar>J</Avatar>
+            <Box marginLeft={'10px'}>
+              <Typography variant="subtitle1">John Doe </Typography>
+              <Typography variant="subtitle2" color="text.secondary">
+                March 15, 2024
+              </Typography>
+            </Box>
+          </Box>
+          <Box>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Box>
         <Typography variant="body2" color="text.primary">
           Anyone with a head on your sholders should avoid this kind of foolery
           at any cost. I bet your bottom dollar that 3 out of the 6 minimum of
@@ -32,11 +96,8 @@ export default function Comments() {
           traffic and more.
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small" sx={{ color: 'grey' }}>
-          Reply
-        </Button>{' '}
-      </CardActions>
+
+      {renderMenu}
     </Card>
   )
 }
