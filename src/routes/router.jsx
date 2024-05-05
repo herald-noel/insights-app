8a2705f841323bac59131da90d0f2c7cb2830a23
herald-noel <bontilao.heraldnoel@gmail.com>
2024-05-05 17:00:45 +0800

    Restrict unauthenticated users to access registered users' routes

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,19 +1,22 @@
-import { BrowserRouter as Router, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPageRoutes from './LandingPageRoutes'
 import HomeRoutes from './HomeRoutes'
 import NotificationRoutes from './NotificationRoutes'
 import BlogRoutes from './BlogRoutes'
 import CreateBlogRoutes from './CreateBlogRoutes'
+import PrivateUserRoutes from './PrivateUserRoutes'
 
 const routes = () => {
   return (
     <Router>
       <Routes>
+        <Route element={<PrivateUserRoutes />}>
+          {HomeRoutes}
+          {NotificationRoutes}
+          {BlogRoutes}
+          {CreateBlogRoutes}
+        </Route>
         {LandingPageRoutes}
-        {HomeRoutes}
-        {NotificationRoutes}
-        {BlogRoutes}
-        {CreateBlogRoutes}
       </Routes>
     </Router>
   )
