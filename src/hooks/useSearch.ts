fb6c7d027c906d0465164c569542382d87d48b25
harknoel <bontilao.harknoel@gmail.com>
2024-05-11 21:22:47 +0800

    Fix searching from a specific blog post

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,13 +1,16 @@
 import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux'
 import { RootState, AppDispatch } from '../reducer/store'
 import { searchKey } from '../reducer/search/searchSlice'
+import { useNavigate } from 'react-router-dom'
 
 const useSearch = () => {
   const dispatch = useDispatch<AppDispatch>()
   const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+  const navigate = useNavigate()
 
   const setSearch = (keyword: string) => {
     dispatch(searchKey(keyword))
+    navigate('/home')
   }
 
   const getSearch = () => {
