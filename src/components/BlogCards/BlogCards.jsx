b28304b91c5f4bf592734ff5f42d00e5a87f46b5
luna-wee <83816898+herald-noel@users.noreply.github.com>
2024-05-13 12:40:46 +0800

    Fix blog cards not updating

--- src/components/BlogCards/BlogCards.jsx
+++ src/components/BlogCards/BlogCards.jsx
@@ -6,8 +6,11 @@ import useFetch from '../../hooks/useFetch'
 import { REQUEST } from '../../data/requests.constants'
 import { useEffect, useState } from 'react'
 import useSearch from '../../hooks/useSearch'
+import { useLocation } from 'react-router-dom'
+import { PATH } from '../../data/paths'
 
 const BlogCards = (props) => {
+  const location = useLocation()
   const { url, isNewPostBtn } = props
   const [pageNumber, setPageNumber] = useState(1)
   const [params, setParams] = useState({})
@@ -26,6 +29,12 @@ const BlogCards = (props) => {
   useEffect(() => {
     fetchData()
   }, [params])
+
+  useEffect(() => {
+    if (location.pathname === PATH.HOME) {
+      fetchData()
+    }
+  }, [location])
 
   useEffect(() => {
     setParams({ page: pageNumber - 1, query: search })
