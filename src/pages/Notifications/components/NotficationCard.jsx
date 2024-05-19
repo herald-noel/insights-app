967ba9dfd95c3076f52d7a4d38ba08009850d6d7
harknoel <bontilao.harknoel@gmail.com>
2024-05-20 04:32:56 +0800

    Implement dynamic blog for notifications

--- src/pages/Notifications/components/NotficationCard.jsx
+++ src/pages/Notifications/components/NotficationCard.jsx
@@ -1,72 +1,64 @@
 import PropTypes from 'prop-types'
-import { Box, Typography, Avatar, Card } from '@mui/material'
+import { Box, Card, CardContent, Avatar, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
+import { format } from 'date-fns'
 
-const NotificationCard = ({ blogTitle, comment }) => {
+const NotificationCard = ({ blogData }) => {
   return (
-    <Typography component="span" variant="body1">
-      <Card
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          padding: 2,
-          marginBottom: 2,
-        }}
-      >
-        <Avatar
-          sx={{
-            width: 48,
-            height: 48,
-            marginRight: 2,
-            bgcolor: 'primary.main',
-            color: 'common.white',
-          }}
-        >
-          {`${comment.user.firstname[0]}${comment.user.lastname[0]}`}
-        </Avatar>
-        <Box flex={1}>
-          <Box display="flex" alignItems="center" marginBottom={1}>
-            <Typography variant="subtitle1" component="span">
-              <Link
-                to={`/profile/${comment.user.userId}`}
-                style={{ textDecoration: 'none', color: 'inherit' }}
-              >
-                {`${comment.user.firstname} ${comment.user.lastname}`}
-              </Link>
-            </Typography>
-            <Typography variant="body2" color="text.secondary" marginLeft={1}>
-              commented on your post
-            </Typography>
-            <Typography
-              variant="body2"
-              color="text.primary"
-              marginLeft={1}
-              fontWeight="bold"
+    <Box sx={{ width: '100%' }}>
+      {blogData &&
+        blogData.map((blog) => (
+          <Card key={blog.blogId} sx={{ mb: 2 }}>
+            <Link
+              to={`/blog/${blog.blogId}`}
+              style={{ textDecoration: 'none', color: 'inherit' }}
             >
-              {blogTitle}
-            </Typography>
-          </Box>
-          <Typography variant="body2" color="text.secondary">
-            {comment.content}
-          </Typography>
-        </Box>
-      </Card>
-    </Typography>
+              <CardContent sx={{ display: 'flex', alignItems: 'center' }}>
+                <Avatar sx={{ bgcolor: 'primary.main', mr: 2 }}>
+                  {`${blog.author.firstname[0]}${blog.author.lastname[0]}`}
+                </Avatar>
+                <Box flex={1}>
+                  <Box display="flex" alignItems="center" mb={1}>
+                    <Typography variant="subtitle1" component="span">
+                      {`${blog.author.firstname} ${blog.author.lastname}`}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary" ml={1}>
+                      created a new post
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="text.primary"
+                      ml={1}
+                      fontWeight="bold"
+                    >
+                      {blog.title}
+                    </Typography>
+                  </Box>
+                  <Typography variant="body2" color="text.secondary">
+                    {`Published on ${format(new Date(blog.createdAt), 'MMMM d, yyyy')}`}
+                  </Typography>
+                </Box>
+              </CardContent>
+            </Link>
+          </Card>
+        ))}
+    </Box>
   )
 }
 
 NotificationCard.propTypes = {
-  blogTitle: PropTypes.string.isRequired,
-  comment: PropTypes.shape({
-    commentId: PropTypes.number.isRequired,
-    user: PropTypes.shape({
-      userId: PropTypes.number.isRequired,
-      firstname: PropTypes.string.isRequired,
-      lastname: PropTypes.string.isRequired,
-    }).isRequired,
-    content: PropTypes.string.isRequired,
-    createdAt: PropTypes.string.isRequired,
-  }).isRequired,
+  blogData: PropTypes.arrayOf(
+    PropTypes.shape({
+      blogId: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      createdAt: PropTypes.string.isRequired,
+      author: PropTypes.shape({
+        userId: PropTypes.number.isRequired,
+        firstname: PropTypes.string.isRequired,
+        lastname: PropTypes.string.isRequired,
+      }).isRequired,
+    })
+  ),
 }
 
 export default NotificationCard
--- src/pages/Notifications/components/NotificationTabs.jsx
+++ src/pages/Notifications/components/NotificationTabs.jsx
@@ -1,36 +1,27 @@
 import { Box } from '@mui/material'
 import NotificationCard from './NotficationCard'
-
+import useFetch from '../../../hooks/useFetch'
+import { useEffect, useState } from 'react'
+import { REQUEST } from '../../../data/requests.constants'
 const NotificationTabs = () => {
+  const [responseData, setResponseData] = useState([])
+  const url = '/notifications/all'
+
+  const { data, fetchData } = useFetch(url, REQUEST.GET)
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  useEffect(() => {
+    setResponseData(data)
+    console.log(responseData)
+  }, [data])
+
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ p: 3 }}>
-        <NotificationCard
-          blogTitle="My Awesome Blog Post"
-          comment={{
-            commentId: 1,
-            user: {
-              userId: 1,
-              firstname: 'John',
-              lastname: 'Doe',
-            },
-            content: 'This is a great post! Keep up the good work.',
-            createdAt: '2023-05-12T10:30:00.000Z',
-          }}
-        />
-        <NotificationCard
-          blogTitle="My Awesome Blog Post"
-          comment={{
-            commentId: 1,
-            user: {
-              userId: 1,
-              firstname: 'John',
-              lastname: 'Doe',
-            },
-            content: 'This is a great post! Keep up the good work.',
-            createdAt: '2023-05-12T10:30:00.000Z',
-          }}
-        />
+        <NotificationCard blogData={responseData} />
       </Box>
     </Box>
   )
