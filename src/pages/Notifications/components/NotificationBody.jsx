9a8969ca3a455c6777d48758044ae5139d0f96ef
harknoel <bontilao.harknoel@gmail.com>
2024-05-20 11:05:33 +0800

    Add blog introduction

--- src/pages/Notifications/components/NotificationBody.jsx
+++ src/pages/Notifications/components/NotificationBody.jsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Typography } from '@mui/material'
+import { Box, Divider, Typography, Grid } from '@mui/material'
 import NotificationTabs from './NotificationTabs'
 import useFetch from '../../../hooks/useFetch'
 import { useEffect } from 'react'
@@ -16,18 +16,34 @@ const NotificationBody = () => {
 
   return (
     <Box sx={{ p: 2 }}>
-      <Box sx={{ marginBottom: 4 }}>
-        <Typography variant="h4" gutterBottom>
-          Notifications
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          What&apos;s New
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          The latest releases from publishers you follow.
-        </Typography>
-      </Box>
-      <Divider />
+      <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h4" gutterBottom>
+              Notifications
+            </Typography>
+          </Grid>
+          <Grid item xs={12} container spacing={2}>
+            <Grid item xs={12} md={6} sx={{ mt: 1 }}>
+              <Typography variant="body1" gutterBottom>
+                What&apos;s New
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                The latest releases from publishers you follow.
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              display="flex"
+              justifyContent="flex-end"
+              alignItems="center"
+              sx={{ mt: 1 }}
+            >
+            </Grid>
+          </Grid>
+        </Grid>
+      <Divider sx={{my: 2}} />
       <NotificationTabs />
     </Box>
   )
